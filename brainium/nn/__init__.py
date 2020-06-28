19f9a97a7bb73862b41b1dd612fcc5ea787bc2e9
Hieu Tr. Pham <hieupt.ai@gmail.com>
2020-06-28 16:45:37 +0700

    Initialize nn package

---
+++ brainium/nn/__init__.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------------------------------------------------
+#  Copyright (c) 2020, Hieu Tr. Pham. All rights reserved.                                                             -
+#  ---------------------------------------------------------------------------------------------------------------------
+#  Licensed under CC BY-NC-SA 4.0.                                                                                     -
+#  You can use and adapt materials for non-commercial purposes as long as                                              -
+#  giving appropriate credit by citing the authors. If you adapt the materials,                                        -
+#  you must distribute your contributions under the same license as the original.                                      -
+# ----------------------------------------------------------------------------------------------------------------------
+from __future__ import absolute_import
